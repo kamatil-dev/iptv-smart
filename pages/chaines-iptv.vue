7ff140c16288fff2e0d96d8e4370c983b6916d9f
kamatil-dev <karim.amahtil@gmail.com>
2023-08-12 03:41:25 +0100

    add content

---
+++ pages/chaines-iptv.vue
@@ -0,0 +1,46 @@
+<template>
+  <section
+    class="relative flex items-center justify-center h-screen overflow-hidden"
+  >
+    <video
+      autoplay
+      loop
+      muted
+      class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+    >
+      <source src="/third.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </section>
+  <section class="bg-black text-white mx-auto px-4 py-32">
+    <div class="flex flex-col gap-5 max-w-2xl text-center mx-auto">
+      <h3 class="text-4xl md:text-5xl font-bold">
+        Tout votre contenu disponible sur une plateforme unique
+      </h3>
+      <p class="text-xl">
+        Profitez de l'accès complet à la vaste bibliothèque de streaming de
+        Hulu, comprenant vos sports en direct préférés, les dernières nouvelles,
+        les événements incontournables et les émissions en cours.
+      </p>
+      <NuxtLink
+        class="mt-8 w-fit mx-auto rounded-xl font-bold bg-fuchsia-900 px-12 py-3 text-white shadow hover:bg-fuchsia-950 focus:outline-none focus:ring ring-[#4c35e3] active:bg-fuchsia-950"
+        to="/#abonnement"
+      >
+        JE COMMANDE MAINTENANT!
+      </NuxtLink>
+    </div>
+  </section>
+  <img
+    class="pl-6 block h-fit w-full"
+    src="/image-section--1536x936.png"
+    alt=""
+  />
+  <img class="block h-fit w-full" src="/section-3-smar.png" alt="" />
+  <img class="block h-fit w-full" src="/section-4-smart-1536x726.png" alt="" />
+</template>
+
+<script setup>
+useHead({
+  title: "Chaines IPTV",
+});
+</script>
---
+++ pages/installation.vue
@@ -0,0 +1,35 @@
+<template>
+  <img
+    class="block h-fit w-full my-20"
+    src="/installation-sldier-1-1.png"
+    alt=""
+  />
+  <section class="bg-black text-white p-16 md:p-24 space-y-6">
+    <h3 class="text-4xl md:text-5xl font-bold">
+      Nous mettons à votre disposition tous les abonnements IPTV
+    </h3>
+    <p class="text-xl">
+      Notre abonnement IPTV est compatible avec toutes les applications
+      disponibles telles que IPTV Smarters Pro, les boîtiers Android, les Smart
+      TVs, Apple TV, MAG, ainsi que les applications Smart IPTV, SET IPTV, Net
+      IPTV et IPTV Smarters, peu importe la plateforme.
+    </p>
+    <NuxtLink
+      class="mt-8 block w-fit rounded-xl font-bold bg-fuchsia-900 px-12 py-3 text-white shadow hover:bg-fuchsia-950 focus:outline-none focus:ring ring-[#4c35e3] active:bg-fuchsia-950"
+      to="/#abonnement"
+    >
+      Accédez à l'IPTV dès maintenant
+    </NuxtLink>
+  </section>
+  <img
+    class="block h-fit w-full"
+    src="/installation-sldier-3-1536x850.png"
+    alt=""
+  />
+</template>
+
+<script setup>
+useHead({
+  title: "Installation",
+});
+</script>
